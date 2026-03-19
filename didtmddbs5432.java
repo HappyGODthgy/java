ba984e6ee160e653d674c28831a6cbf2797919c7
HappyGODthgy <daejung1624@dge.go.kr>
2026-03-19 10:31:57 +0900

    Add files via upload

---
+++ didtmddbs5432.java
@@ -0,0 +1,9 @@
+package jump2java;
+
+public class didtmddbs5432 {
+	StringBuffer sb = new StringBuffer();
+	sb.append("jump to java");
+	sb.insert(0, "hello ");
+	System.out.println(sb.toString());
+	}
+}
