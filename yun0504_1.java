03943a5e7359c0de8b3c70a3878574a6ab0d3875
HappyGODthgy <daejung1624@dge.go.kr>
2026-04-02 09:28:25 +0900

    Add files via upload

---
+++ yun0504_1.java
@@ -0,0 +1,22 @@
+package jump2java;
+	
+	class Updater {
+		void update(Counter counter) {
+			counter.count++;
+	}
+}
+
+class Counter {
+	int count = 0;
+}
+
+public class yun0504_1 {
+	public static void main(String[] args) {
+		Counter myCounter = new Counter();
+		System.out.println("before update:"+myCounter.count);
+		Updater myUpdater = new Updater();
+		myUpdater.update(myCounter);
+		System.out.println("after update:"+myCounter.count);
+		
+	}
+}
